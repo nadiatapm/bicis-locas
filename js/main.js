47bc92557742f5d21147d8bca60a8ea8d5052f24
nadia <nadiatap@gmail.com>
2016-10-28 22:58:30 -0300

    ultimos cambios en seleccion  bici js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,10 @@ function validateForm(){
 		var name = document.getElementById("name").value;	
 		if(name == null || name.length == 0 ){
 			alert('Campo obligatorio');
-			return false;
 		}else if(!(name[0] == name[0].toUpperCase())){
 			alert('Nombre debe comenzar en Mayusculas');
-			return false;
-		}else if(/^[a-zA-z\s\ñ\Ñ]+$/){
+		}else if(/[0-9]/.test(name)){
 			alert('Nombre solo debe tener letras');
-			return false;
 		}else{
 			return true;
 		}
@@ -22,34 +19,31 @@ validarNombre();
 
 	function validarApellido(){
 		var lastname = document.getElementById("lastname").value;
-		if(lastname == null || lastname.length == 0){
+		if(lastname == null || lastname.length == 0 ){
 			alert('Campo obligatorio');
-			return false;
 		}else if(!(lastname[0] == lastname[0].toUpperCase())){
 			alert('Nombre debe comenzar con Mayuscula');
-			return false;
-		}else if(/^[a-zA-z\s\ñ\Ñ]+$/){
+		}else if(/[0-9]/.test(lastname)){
 			alert('Nombre solo debe tener letras');
-			return false;
 		}else{
 			return true;
 		}
 	}
 validarApellido();
 
-	//validar campo email tenga formato valido .
-	/*function validarEmail(){
+	//validar campo email tenga formato valido 
+	function validarEmail(){
 		var caracter =  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;  
 		var correo = document.getElementById("input-email").value;
 		if(correo === null || correo.length === 0){
-			alert("la dirección de email" + valor + "Es correcto");
-		else()
-			alert("La dirección de email es incorrecta.");
-		}else{
+			alert("Ingresar email");
+			return false
+		}else if(caracter.test(correo)){
 			return true;
-		}
-	}	
-validarEmail();	*/
+		
+	    }
+    }
+validarEmail();	
 
 	//el campo password debe tener al menos 6 caracteres 
 	//password no debe ser igual a "password" o "123456" o "0987654"
@@ -57,10 +51,8 @@ validarEmail();	*/
 		var clave = document.getElementById('input-password').value;
 		if(clave === null || clave.length === 0){
 			alert('Ingrese password');
-			return false;	
 		}else if(clave.length<6){
 			alert ('password debe ser de 6 caracteres');
-			return false;
 		}else if(clave === '123456' || clave === '098765' || clave === 'password'){
 			alert('password invalida')
 		}else{
@@ -71,14 +63,14 @@ validarClave();
 
 	//el valor seleccionado bicis debe ser una opcion presentada
 	function seleccionBici(){
-		var bici = document.getElementByTagName('select').value;
-		for(var i = 0; i < bici.length; i++){
-			if(bici[i].value === "0"){
-				alert('Seleciona tu bici!!!!');
+		var elige = document.getElementsByTagName('select')[0];
+			for(var i = 0; i < elige.length; ++i){
+				if(elige[i].value === "0"){
+					alert('Seleciona tu bici!!!!');
 				return false;
+				}
 			}
 			return true;
-		}
 	}
 seleccionBici();
 }
